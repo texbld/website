96e3bf61bb8b410700f69617d66c5c7952d0e2ba
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-06-25 23:43:40 -0400

    code splitting works

--- src/docs/routes.ts
+++ src/docs/routes.ts
@@ -1,36 +1,36 @@
-import { lazy } from "react";
+import React, { lazy } from "react";
 
 interface Route {
   path: string;
   title: string;
-  component: string;
+  component: React.LazyExoticComponent<any>;
 }
 
 const routes: Route[] = [
   {
     path: "",
     title: "Getting Started",
-    component: "Gettingstarted",
+    component: lazy(() => import(`./markdown/Gettingstarted.mdx`)),
   },
   {
     path: "0.2/project",
     title: "Project (0.2-dev)",
-    component: "0.2/project",
+    component: lazy(() => import(`./markdown/0.2/project.mdx`)),
   },
   {
     path: "0.2/image",
     title: "Image (0.2-dev)",
-    component: "0.2/image",
+    component: lazy(() => import(`./markdown/0.2/image.mdx`)),
   },
   {
     path: "0.1/project",
     title: "Project (0.1)",
-    component: "0.1/project",
+    component: lazy(() => import(`./markdown/0.1/project.mdx`)),
   },
   {
     path: "0.1/image",
     title: "Images (0.1)",
-    component: "0.1/image",
+    component: lazy(() => import(`./markdown/0.1/image.mdx`)),
   },
 ];
 
@@ -39,6 +39,6 @@ const exp = routes.map((route) => ({
   component: lazy(() => import(`./markdown/${route.component}.mdx`)),
 }));
 
-export default exp;
+export default routes;
 
 //console.log(import.meta.glob("./markdown/**/*.mdx"));
