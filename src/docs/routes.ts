cb683f6fbbbf24b2cf9c139f8c4450c1943605e2
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-06-25 23:00:40 -0400

    routes extension

--- src/docs/routes.ts
+++ src/docs/routes.ts
@@ -10,35 +10,35 @@ const routes: Route[] = [
   {
     path: "",
     title: "Getting Started",
-    component: "Gettingstarted.mdx",
+    component: "Gettingstarted",
   },
   {
     path: "0.2/project",
     title: "Project (0.2-dev)",
-    component: "0.2/project.mdx",
+    component: "0.2/project",
   },
   {
     path: "0.2/image",
     title: "Image (0.2-dev)",
-    component: "0.2/image.mdx",
+    component: "0.2/image",
   },
   {
     path: "0.1/project",
     title: "Project (0.1)",
-    component: "0.1/project.mdx",
+    component: "0.1/project",
   },
   {
     path: "0.1/image",
     title: "Images (0.1)",
-    component: "0.1/image.mdx",
+    component: "0.1/image",
   },
 ];
 
 const exp = routes.map((route) => ({
   ...route,
-  component: lazy(() => import(`./markdown/${route.component}`)),
+  component: lazy(() => import(`./markdown/${route.component}.mdx`)),
 }));
 
 export default exp;
 
-console.log(import.meta.glob("./markdown/**/*.mdx"));
+//console.log(import.meta.glob("./markdown/**/*.mdx"));
